73de1f377d20be103956f71749f61390fa468a00
rick <ricklivernois@gmail.com>
2025-06-03 11:31:54 -0500

    Meta Data and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    integrations: [mdx(), sitemap()],
     site: 'https://wliverno.github.io/',
     base: '/',
     outDir: './dist',
